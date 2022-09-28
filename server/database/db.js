0efb6857937bcc6c3645cc9f26826beacf48acf1
Rudy Zourane <rz@gmail;com>
2022-09-28 19:25:44 +0200

    Update package

--- server/database/db.js
+++ server/database/db.js
@@ -1,21 +1,24 @@
 let uri = null;
 const mongoose = require('mongoose');
 const debug = process.env.NODE_ENV === 'dev';
+const bdd = process.env.BDD;
+const cluster = process.env.CLUSTER;
+const bddFolder = process.env.bddFolder;
 const options = {};
 
 function connectDB (who = 'unknown') {
   if (process.env.NODE_ENV === 'dev') {
     console.log('DEV MODE !')
-    uri = 'mongodb://127.0.0.1:27017/Book';
+    uri = `mongodb://127.0.0.1:27017/${bddFolder}`;
   } else {
     console.log('NOT IN DEV MODE !');
-    uri = 'mongodb+srv://db:pcwd';
+    uri = `mongodb+srv://${bddFolder}:${bdd}@${cluster}.mongodb.net/Book`;
   }
 
-  mongoose.connect(uri).then(
+  mongoose.connect(uri, { useNewUrlParser: true }).then(
     () => { console.log(`Connexion à MongoDB réussie ! ${who} est connecté`) },
-    err => { console.log(`Connexion à MongoDB fail ! ${who} n'est pas connecté`) }
-  );
+  err => { console.log(`Connexion à MongoDB fail ! ${who} n'est pas connecté`) }
+);
 }
 
 module.exports = connectDB;
